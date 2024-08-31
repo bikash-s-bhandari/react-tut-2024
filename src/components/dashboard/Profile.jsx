b27bfc647871e25b78d1b774018056115090899d
bikash bhandari <bikas@thefireflytech.com>
2024-08-31 13:36:52 +0545

    use navigation hook added

--- src/components/dashboard/Profile.jsx
+++ src/components/dashboard/Profile.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; 
 
 function Profile() {
-  return <h3>Profile Page</h3>;
+  const navigate = useNavigate();  // Initialize useNavigate hook
+
+  const goToHDocumentation = () => {
+      navigate('/docs');  
+    };
+  return (
+    <>
+  <h3>Profile Page</h3>
+  <button onClick={goToHDocumentation} style={{ padding: '0.5rem 1rem', marginTop: '1rem', cursor: 'pointer' }}>
+        Go To Documentation
+      </button>
+  </>
+);
 }
 
 export default Profile;
