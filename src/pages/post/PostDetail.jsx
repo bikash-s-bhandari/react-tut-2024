e6a045a14104248e2fe5a62997bc9ebf7cd542ee
bikash bhandari <bikas@thefireflytech.com>
2024-09-02 16:36:56 +0545

    use location

--- src/pages/post/PostDetail.jsx
+++ src/pages/post/PostDetail.jsx
@@ -1,9 +1,33 @@
-import { useParams } from 'react-router-dom';
+import { useParams,useLocation } from "react-router-dom";
 
 const PostDetail = () => {
-    const { id,slug } = useParams(); 
-    return <>Post Detail of {id} {slug??''} </>;
-  };
+  const { id, slug } = useParams();
+
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
   
-  export default PostDetail;
-  
+  const name = queryParams.get('name'); //http://localhost:3001/posts/1?name=bikash&age=32
+  const age=queryParams.get('age');
+
+  console.log(location,name,age);
+
+
+  return (
+    <>
+      <div>
+        <h1>useParams hook</h1>
+        Post Detail of {id} {slug ?? ""}
+      </div>
+
+      <div>
+        <h1>Use Location Hook </h1>
+      <p>Current Pathname: {location.pathname}</p>
+      <p>Search String: {location.search}</p>
+      <p>Hash: {location.hash}</p>
+    </div>
+    </>
+  );
+};
+
+export default PostDetail;
